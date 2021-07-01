71e00437d577856d17f19d4208f180a53d6660ce
Nicole Gathany <nicole.gathany@gmail.com>
2021-07-01 03:29:39 +0000

    just some call stack game stuff to understand

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var maxSubArray = function(nums) {
 
 console.log(maxSubArray([1]));
 console.log(maxSubArray([5,4,-1,7,8]));
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
 //i=0, nums[0] is 5
 //prev = Math.max(0+5, 5);
 //0+5=5
